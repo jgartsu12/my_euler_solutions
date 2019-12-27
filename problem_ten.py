2de303d519dc237d5a36ec881b67ce2a8ef8ccdc
jgartsu12 <jgartsu12@gmail.com>
2019-12-27 18:15:43 -0500

    solved

---
+++ problem_ten.py
@@ -0,0 +1,48 @@
+"""
+Summation of primes
+
+Problem 10
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+"""
+
+
+prime_list = []
+
+def prime_find_sum():
+    prime_range = range(2, int(2e6))
+    for x in prime_range:
+        if x > 1:
+            for num in range(2, x):
+                if (x % num) == 0:
+                    break
+            else:
+                prime_list.append(x)
+    print(prime_list)
+    # print(sum(prime_list))
+
+
+prime_find_sum()
+
+'''
+# case example
+
+prime_list = []
+
+def prime_find_sum():
+    prime_range_ten = range(2,10)
+    for x in prime_range_ten:
+        if x > 1:
+            for num in range(2, x):
+                if (x % num) == 0:
+                    break
+            else:
+                prime_list.append(x)
+
+    print(sum(prime_list))
+
+
+prime_find_sum()
+'''
+
